0694af58f468d8f69f7bf38908e5d1f9049d3eef
Daniil Beltyukov <beltyukovdanil@gmail.com>
2019-01-14 21:20:18 +0300

    Remove registry

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
+/* eslint-disable object-curly-newline */
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
