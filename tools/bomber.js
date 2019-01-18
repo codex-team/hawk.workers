67ebaf5156304b68881647fc4bba8ea18650c766
Daniil Beltyukov <beltyukovdanil@gmail.com>
2019-01-18 18:38:15 +0300

    Use library to catch errors

--- tools/bomber.js
+++ tools/bomber.js
@@ -3,7 +3,8 @@
  */
 
 /* eslint-disable require-jsdoc*/
-let request = require('request-promise-native');
+const Hawk = require('@codexteam/hawk.nodejs');
+
 /**
  * Hawk catcher url
  */
@@ -14,63 +15,24 @@ const CATCHER_URL = process.env.CATCHER_URL || 'http://localhost:3000/catcher';
  */
 const CATCHER_TOKEN = process.env.CATCHER_TOKEN || 'randomtoken';
 
-/**
- * Hawk sender ip
- */
-const CATCHER_SENDER = process.env.CATCHER_SENDER || '127.0.0.1';
-
-/**
- * Cather type — nodejs
- * @constant
- */
-const CATCHER_TYPE = 'errors/nodejs';
-
-/**
- * Catches and sends exception to hawk
- *
- * @param {Error} error - Error instance
- * @param {Object} [custom={}] - Custom data to send
- * @returns Promise<void>
- */
-const catchException = async (error, custom = {}) => {
-  const payload = {
-    message: error.name + ': ' + error.message,
-    type: error.name,
-    stack: error.stack,
-    time: new Date().toISOString(),
-
-    // custom params
-    comment: custom.comment || ''
-  };
-
-  const data = {
-    token: CATCHER_TOKEN,
-    sender: {ip: CATCHER_SENDER},
-    /* eslint-disable-next-line */
-    catcher_type: CATCHER_TYPE,
-    payload
-  };
-
-  const resp = await request.post(CATCHER_URL, {
-    json: true, body: data, resolveWithFullResponse: true
-  });
-
-  if (resp.statusCode === 200) {
-    console.log('Sent');
-  } else {
-    console.error(`Error while sending:\n${resp}`);
-  }
-};
+const hawkCatcher = Hawk({
+  url: CATCHER_URL,
+  accessToken: CATCHER_TOKEN
+});
 
 const main = async () => {
   function namedFunc() {
     try {
-      console.log('Do smt useful');
+      console.log('Named func');
       /* eslint-disable-next-line */
       kek();
       return true;
     } catch (e) {
-      catchException(e, {comment: 'Exception in namedFunc'}).catch(err => console.error(err));
+      hawkCatcher.catchException(e, {comment: 'Exception in namedFunc'}, (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+      });
     }
   }
 
@@ -82,7 +44,11 @@ const main = async () => {
       /* eslint-disable-next-line */
       return true;
     } catch (e) {
-      catchException(e, {comment: 'Exception in anonFunc'}).catch(err => console.error(err));
+      hawkCatcher.catchException(e, {comment: 'Exception in anonFunc'}, (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body);
+      });
     }
   }, parseInt(Math.random() * 1000, 10));
 };
