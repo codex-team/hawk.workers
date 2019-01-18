70dcabf0c418b54e57775b9502f5ca09f0530f77
Daniil Beltyukov <beltyukovdanil@gmail.com>
2019-01-18 19:54:36 +0300

    Add argv parser

--- tools/nodejs/bomber.js
+++ tools/nodejs/bomber.js
@@ -1,8 +1,23 @@
+#!/usr/bin/env node
+
 /**
  * Error bomber for nodejs hawk catcher
  */
 
 /* eslint-disable require-jsdoc*/
+var argv = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .command('once', 'send error once')
+  .example('$0 once', 'send error once and exit')
+  .command('interval [seconds]', 'send errors every interval time')
+  .example('$0 interval 5', 'send error every 5 seconds')
+  .demandCommand(1, 'You need at least one command before moving on')
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+const path = require('path');
+
+require('dotenv').config({path: path.resolve(__dirname, '.env')});
 const Hawk = require('@codexteam/hawk.nodejs');
 
 /**
@@ -29,28 +44,28 @@ const main = async () => {
       return true;
     } catch (e) {
       hawkCatcher.catchException(e, {comment: 'Exception in namedFunc'}, (error, response, body) => {
-        console.log('error:', error); // Print the error if one occurred
+        if (error) {
+          console.log('error:', error); // Print the error if one occurred
+        }
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        if (body) {
+          console.log('body:', body); // Print the HTML for the Google homepage.
+        }
       });
     }
   }
 
-  setInterval(namedFunc, parseInt(Math.random() * 1000, 10));
-  setInterval(() => {
-    try {
-      console.log('Anon func');
-      throw Error('WHOOPS');
-      /* eslint-disable-next-line */
-      return true;
-    } catch (e) {
-      hawkCatcher.catchException(e, {comment: 'Exception in anonFunc'}, (error, response, body) => {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body);
-      });
+  const command = argv._[0];
+
+  if (command === 'once') {
+    namedFunc();
+  } else if (command === 'interval') {
+    if (argv.seconds) {
+      const interval = parseInt(argv.seconds * 1000);
+
+      setInterval(namedFunc, interval);
     }
-  }, parseInt(Math.random() * 1000, 10));
+  }
 };
 
 main();
