fa7fb62f8fa577b8b614bf438f0c27d9d79eaac5
dyadyaJora <ja_massa97@mail.ru>
2019-01-23 07:25:01 +0300

    simple model for 'payload'

---
+++ db/models/data.js
@@ -0,0 +1,25 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+/**
+ * Payload data format
+ */
+let dataSchema = new Schema({
+  /**
+   * Event data
+   */
+  payload: {
+    type: Schema.Types.Mixed,
+    required: true
+  }
+
+  /**
+   * @optional
+   * meta info
+   */
+  meta: Schema.Types.Mixed
+});
+
+const Data = mongoose.model('Data', dataSchema);
+
+module.exports = Data;
