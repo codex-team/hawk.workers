4ac32e6f9fffaae582bd3ab8ae0daef80a9ea97a
Murod Khaydarov <murod.haydarov@gmail.com>
2019-08-25 00:40:16 +0300

    mongo migrator initial

---
+++ migrate-mongo-config.js
@@ -0,0 +1,24 @@
+// In this file you can configure migrate-mongo
+const config = {
+  mongodb: {
+    url: process.env.MONGO_DNS,
+    databaseName: process.env.MONGO_DBNAME,
+
+    options: {
+      useNewUrlParser: true // removes a deprecation warning when connecting
+      /*
+       *   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+       *   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+       */
+    }
+  },
+
+  // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
+  migrationsDir: 'migrations',
+
+  // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
+  changelogCollectionName: 'migration-schema'
+};
+
+// Return the config as a promise
+module.exports = config;
