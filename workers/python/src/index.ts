8db091ef47624a178d725290c715cdbfe6f7a939
N0str <ntpcp@yandex.ru>
2019-08-30 03:19:44 +0300

    Add python worker

---
+++ workers/python/src/index.ts
@@ -0,0 +1,54 @@
+import * as WorkerNames from '../../../lib/workerNames';
+import { DatabaseController } from '../../../lib/db/controller';
+import { PythonEventWorkerTask} from '../types/python-event-worker-task';
+import { EventWorker } from '../../../lib/event-worker';
+import { GroupWorkerTask } from '../../grouper/types/group-worker-task';
+import * as pkg from '../package.json'
+
+/**
+ * Worker for handling Python events
+ */
+export default class PythonEventWorker extends EventWorker {
+  /**
+   * Worker type (will pull tasks from Registry queue with the same name)
+   */
+  public readonly type: string = pkg.workerType;
+
+  /**
+   * Database Controller
+   */
+  private db: DatabaseController = new DatabaseController();
+
+  constructor(){
+    super();
+  }
+
+  /**
+   * Start consuming messages
+   */
+  public async start(): Promise<void> {
+    await this.db.connect();
+    await super.start();
+  }
+
+  /**
+   * Finish everything
+   */
+  public async finish(): Promise<void> {
+    await super.finish();
+    await this.db.close();
+  }
+
+  /**
+   * Message handle function
+   */
+  public async handle(event: PythonEventWorkerTask): Promise<void> {
+    await super.handle(event);
+
+    await this.addTask(WorkerNames.GROUPER, {
+      projectId: this.projectId,
+      catcherType: this.type,
+      event: event.payload
+    } as GroupWorkerTask);
+  }
+}
