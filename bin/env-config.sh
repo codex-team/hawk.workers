b38965301074a922646b73a8fff2162a2fa04954
Danil Beltyukov <beltyukovdanil@gmail.com>
2019-08-30 22:18:29 +0300

    set shebang

--- bin/env-config.sh
+++ bin/env-config.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 # For TRAVIS
-# Copies files with environment variables from .env.sample at .env 
+# Copies files with environment variables from .env.sample at .env
 # for all subfolders where it is exist
 # (can not use --execdir in travis https://github.com/travis-ci/travis-ci/issues/2811)
 
