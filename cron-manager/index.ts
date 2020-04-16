cde0df23472192a94623e0df531dca6b6732b0d1
Nikita Melnikov <nikmel2803@gmail.com>
2020-04-16 19:47:47 +0300

    eslint fix

--- cron-manager/index.ts
+++ cron-manager/index.ts
@@ -1,16 +1,16 @@
 import fs from 'fs';
 import * as yaml from 'yaml';
 import path from 'path';
-import cron from 'cron';
+import { CronJob } from 'cron';
 import { CronManagerConfig } from './types';
 
 const configFile = fs.readFileSync(path.join(__dirname, './config.yml')).toString();
 
 const config = yaml.parse(configFile) as CronManagerConfig;
-const jobs: cron.CronJob[] = [];
+const jobs: CronJob[] = [];
 
 config.tasks.forEach(task => {
-  const job = new cron.CronJob(task.schedule, () => {
+  const job = new CronJob(task.schedule, () => {
     console.log(task.workerName);
   });
 
