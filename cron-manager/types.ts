330fe68c869f0192d1cc353a6c522e55aeab415b
Nikita Melnikov <nikmel2803@gmail.com>
2020-04-23 00:03:21 +0300

    update docs in types.ts

--- cron-manager/types.ts
+++ cron-manager/types.ts
@@ -1,8 +1,29 @@
+/**
+ * Cron manager configuration object
+ */
 export interface CronManagerConfig {
+  /**
+   * Task list to manage
+   */
   tasks: CronManagerTask[];
 }
 
+/**
+ * Task representation
+ */
 export interface CronManagerTask {
-  workerName: string;
+  /**
+   * Worker type to add tasks (equal to the Registry queue name)
+   */
+  workerType: string;
+
+  /**
+   * Cron-like schedule string
+   */
   schedule: string;
+
+  /**
+   * Any data to send with task
+   */
+  payload?: object;
 }
