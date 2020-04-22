903732274d38fa23074c821e8676c02f9e9353a8
Nikita Melnikov <nikmel2803@gmail.com>
2020-04-23 00:11:20 +0300

    fix type error

--- cron-manager/manager.ts
+++ cron-manager/manager.ts
@@ -37,7 +37,7 @@ export default class CronManager {
     this.registryUrl = registryUrl;
     config.tasks.forEach(task => {
       const job = new CronJob(task.schedule, async () => {
-        await this.addTask(task.workerType, task.payload);
+        await this.addTask(task.workerType, task.payload || {});
       });
 
       this.jobs.push(job);
