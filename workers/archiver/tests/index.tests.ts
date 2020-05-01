9b42a9b7a5372f759822e2a19e9f660a7b535c55
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-01 14:45:38 +0300

    add types for asyncForEach

--- workers/archiver/tests/index.tests.ts
+++ workers/archiver/tests/index.tests.ts
@@ -1,3 +1,21 @@
+import { MongoClient } from 'mongodb';
+
 describe('Archiver worker', () => {
-  test('');
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = await connection.db();
+  });
+
+  test('Should correctly remove old events', () => {
+
+  });
+  afterAll(async () => {
+    await connection.close();
+  });
 });
