72cf41ae36ba45815341374791c351b34eff19e9
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-01 14:45:57 +0300

    add types for asyncForEach

--- lib/utils/asyncForEach.ts
+++ lib/utils/asyncForEach.ts
@@ -4,7 +4,10 @@
  * @param array - array to iterate
  * @param callback - callback for processing array items
  */
-export default async function asyncForEach(array, callback): Promise<void> {
+export default async function asyncForEach(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  array: any[], callback: Function
+): Promise<void> {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
