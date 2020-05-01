1451e9c1cc7144b7b788323beb12a64fcbf44575
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-01 14:04:59 +0300

    add docs in env.d.ts

--- workers/env.d.ts
+++ workers/env.d.ts
@@ -1,6 +1,17 @@
+/**
+ * @file Types for process.env
+ */
+
 declare namespace NodeJS {
   export interface ProcessEnv {
+    /**
+     * Database name with events
+     */
     EVENTS_DB_NAME: string;
+
+    /**
+     * Database name with accounts
+     */
     ACCOUNTS_DB_NAME: string;
   }
 }
