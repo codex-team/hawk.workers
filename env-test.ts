14a93fa3a16a1e3b93ab5093ab17e5d09cb28235
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-04 15:29:30 +0300

    fix env vars assigment

--- env-test.ts
+++ env-test.ts
@@ -5,4 +5,4 @@
 import * as dotenv from 'dotenv';
 import { resolve } from 'path';
 
-dotenv.config({ path: resolve(__dirname, '.env.test') });
+Object.assign(process.env, dotenv.config({ path: resolve(__dirname, '.env.test') }).parsed);
