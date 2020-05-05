fabe14bfdff456e5c4004eaf99a1fe5938eb2cb9
Peter Savchenko <specc.dev@gmail.com>
2020-05-05 09:31:52 +0300

    Feature / Telegram worker (#102)
    * Feature / Telegram worker
    
    * lint
    
    * add test
    
    * lint
    
    * rm unused packages
    
    * fix test
    
    * restore dropped test
    
    * rm unused var

---
+++ workers/telegram/src/env.ts
@@ -0,0 +1,9 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+/**
+ * Load local environment configuration
+ */
+const localEnv = dotenv.config({ path: path.resolve(__dirname, '../.env') }).parsed;
+
+Object.assign(process.env, localEnv);
