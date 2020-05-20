20868310ec56d010b0fd2cc0c813696b1c3c5cae
Nikita Melnikov <nikmel2803@gmail.com>
2020-05-20 19:10:23 +0300

    fix build error in accountant worker (#112)

---
+++ lib/types/workspace.ts
@@ -0,0 +1,33 @@
+import { ObjectId } from 'mongodb';
+
+/**
+ * Workspace representation in DataBase
+ */
+interface WorkspaceDBScheme {
+  /**
+   * Workspace's id
+   */
+  _id: ObjectId;
+
+  /**
+   * Workspace's name
+   */
+  name: string;
+
+  /**
+   * Workspace's description
+   */
+  description?: string;
+
+  /**
+   * Workspace's image URL
+   */
+  image?: string;
+
+  /**
+   * Workspace balance
+   */
+  balance: number;
+}
+
+export default WorkspaceDBScheme;
