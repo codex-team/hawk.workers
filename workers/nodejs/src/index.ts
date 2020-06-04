6dcc4e403b04f2f37ec2840a7c295957e97e44f2
Taly <vitalik7tv@yandex.ru>
2020-06-04 03:17:54 +0300

    Initial version

---
+++ workers/nodejs/src/index.ts
@@ -0,0 +1,28 @@
+import { EventWorker } from '../../../lib/event-worker';
+import * as WorkerNames from '../../../lib/workerNames';
+import { GroupWorkerTask } from '../../grouper/types/group-worker-task';
+import * as pkg from '../package.json';
+import { NodeJSEventWorkerTask } from '../types/nodejs-event-worker-task';
+
+/**
+ * Worker for handling NodeJS events
+ */
+export default class NodeJSEventWorker extends EventWorker {
+  /**
+   * Worker type (will pull tasks from Registry queue with the same name)
+   */
+  public readonly type: string = pkg.workerType;
+
+  /**
+   * Message handle function
+   *
+   * @param event - event to handle
+   */
+  public async handle(event: NodeJSEventWorkerTask): Promise<void> {
+    await this.addTask(WorkerNames.GROUPER, {
+      projectId: event.projectId,
+      catcherType: this.type,
+      event: event.payload,
+    } as GroupWorkerTask);
+  }
+}
---
+++ workers/nodejs/types/nodejs-event-worker-task.d.ts
@@ -0,0 +1,27 @@
+import { EventData, EventWorkerTask } from '../../../lib/types/event-worker-task';
+
+/**
+ * Format of task for NodeJS Event Worker
+ */
+export interface NodeJSEventWorkerTask extends EventWorkerTask {
+  /**
+   * Language-specific payload
+   */
+  payload: NodeJSEventPayload;
+}
+
+/**
+ * Describe a context passed from NodeJS Catcher
+ */
+interface NodeJSEventPayload extends EventData {
+  /**
+   * Language-specific useful information from NodeJS Catcher
+   */
+  context: NodeJSEventContext;
+}
+
+/**
+ * Information about client
+ */
+interface NodeJSEventContext {
+}
