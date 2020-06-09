40b07c87aebd393742cbe9ccd59d355976d07682
N0str <ntpcp@yandex.ru>
2020-06-09 23:12:10 +0300

    Add build command. Fix deploy CI

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -2,17 +2,24 @@
 # If you want to change it, check `docker network ls`
 default_network="hawkmono_default"
 
-if [[ "$(docker images -q hawk-workers 2> /dev/null)" == "" ]]; then
-  echo "Not found docker image. Building ..."
-  docker build -t hawk-workers .
-fi
-
 help ()
 {
-    echo "Run example: ./run-in-docker.sh <worker_name>"
-    echo "Available workers:"
-    sed -n -e 's/.*\(hawk-worker-[^"]*\).*/ -> \1/p' package.json
+  echo "Run example: ./run-in-docker.sh <worker_name>"
+  echo "For rebuilding run: ./run-in-docker.sh --build"
+  echo "Available workers:"
+  sed -n -e 's/.*\(hawk-worker-[^"]*\).*/ -> \1/p' package.json
 }
+
+build ()
+{
+  echo "Building hawk-workers..."
+  docker build -t hawk-workers .
+}
+
+if [[ "$(docker images -q hawk-workers 2> /dev/null)" == "" ]]; then
+  echo "Not found docker image"
+  build
+fi
 
 if [ "$#" -eq 0 ]; then
     help
@@ -22,6 +29,8 @@ for arg in "$@"
 do
     if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]
     then help
+    elif [ "$arg" == "--build" ]
+    then build
     else
       docker run --rm -d --network ${default_network} -v $(PWD)/.env:/app/.env --entrypoint /usr/local/bin/node hawk-workers runner.js $arg
     fi
