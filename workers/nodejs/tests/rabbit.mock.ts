e9a1b85dd5b8d3cba4e686d16bf2c8c3b7df2b05
Taly <vitalik7tv@yandex.ru>
2020-06-09 21:05:57 +0300

    NodeJS worker (#116)
    * Initial version
    
    * Add test
    
    * Update event payload type
    
    * Update nodejs-event-worker-task.d.ts
    
    * Update tests
    
    * Return timestamp
    
    * validate function
    
    * Update tests
    
    * Update rabbit.mock.ts

---
+++ workers/nodejs/tests/rabbit.mock.ts
@@ -0,0 +1,24 @@
+import amqp from 'amqplib';
+
+/**
+ * Amqp channel mock
+ */
+export const mockedAmqpChannel = {
+  close: jest.fn(),
+  assertQueue: jest.fn(),
+  prefetch: jest.fn(),
+  consume: jest.fn().mockReturnValue('mockedTag'),
+  sendToQueue: jest.fn(),
+};
+
+/**
+ * Connection object mock for testing work with RabbitMQ
+ */
+const mockedAmqpConnection = {
+  createChannel: (): object => mockedAmqpChannel,
+  close: jest.fn(),
+};
+
+const mockedConnect = amqp.connect as jest.Mock;
+
+mockedConnect.mockResolvedValue(Promise.resolve(mockedAmqpConnection));
