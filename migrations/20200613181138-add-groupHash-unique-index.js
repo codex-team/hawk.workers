62b2fbb3dd7a309e7a87d8277d73e6aeb8af41d5
Nikita Melnikov <nikmel2803@gmail.com>
2020-06-13 22:47:11 +0300

    create migration for adding groupHash unique index

---
+++ migrations/20200613181138-add-groupHash-unique-index.js
@@ -0,0 +1,141 @@
+/**
+ * @file This migration creates indexes for repetition collection on payload.user.id field
+ */
+
+/**
+ * Asynchronous forEach function
+ *
+ * @param array - array to iterate
+ * @param callback - callback for processing array items
+ */
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+/**
+ * Index name for payload.user.id field
+ */
+const indexName = 'groupHashUnique';
+
+module.exports = {
+  async up(db) {
+    const collections = await db.listCollections({}, {
+      authorizedCollections: true,
+      nameOnly: true,
+    }).toArray();
+
+    const targetCollections = [];
+
+    collections.forEach((collection) => {
+      if (/events/.test(collection.name)) {
+        targetCollections.push(collection.name);
+      }
+    });
+
+    console.log('Start adding indexes to groupHash field');
+    console.log(`${targetCollections.length} collections will be updated.`);
+
+    let currentCollectionNumber = 1;
+
+    for (const collectionName of targetCollections) {
+      const hasIndexAlready = await db.collection(collectionName).indexExists(indexName);
+
+      console.log(`${currentCollectionNumber} of ${targetCollections.length} in process.`);
+
+      if (!hasIndexAlready) {
+        const collection = db.collection(collectionName);
+
+        /**
+         * Find duplicated events
+         */
+        const groupedEvents = await collection
+          .aggregate([
+            {
+              $group: {
+                _id: '$groupHash',
+                events: {
+                  $push: {
+                    _id: '$_id',
+                    totalCount: '$totalCount',
+                  },
+                },
+                count: { $sum: 1 },
+              },
+            },
+            {
+              $match: {
+                count: { $gt: 1 },
+              },
+            },
+          ])
+          .toArray();
+
+        /**
+         * Remove duplicated events
+         */
+        await asyncForEach(groupedEvents, async (groupedEvent) => {
+          const originalEvent = groupedEvent.events.pop();
+
+          const totalCount = groupedEvent.events.reduce((acc, val) => acc + val.totalCount - 1, 0);
+          const idsToRemove = groupedEvent.events.map(event => event._id);
+
+          await collection.updateOne({
+            _id: originalEvent._id,
+          }, {
+            $inc: {
+              totalCount: totalCount,
+            },
+          });
+
+          await collection.deleteMany({
+            _id: {
+              $in: idsToRemove,
+            },
+          });
+        });
+
+        await collection.createIndex({
+          groupHash: 1,
+        },
+        {
+          unique: true,
+          name: indexName,
+        });
+        console.log('Create index', collectionName);
+      } else {
+        console.log('Skip', collectionName);
+      }
+
+      currentCollectionNumber++;
+    }
+  },
+  async down(db) {
+    const collections = await db.listCollections({}, {
+      authorizedCollections: true,
+      nameOnly: true,
+    }).toArray();
+
+    const targetCollections = [];
+
+    collections.forEach((collection) => {
+      if (/events/.test(collection.name)) {
+        targetCollections.push(collection.name);
+      }
+    });
+
+    console.log('Start dropping indexes from groupHash field');
+    console.log(`${targetCollections.length} collections will be updated.`);
+
+    let currentCollectionNumber = 1;
+
+    for (const collectionName of targetCollections) {
+      console.log(`${currentCollectionNumber} of ${targetCollections.length} in process.`);
+
+      await db.collection(collectionName).dropIndex(indexName);
+      console.log('Index dropped', collectionName);
+      currentCollectionNumber++;
+    }
+  },
+};
