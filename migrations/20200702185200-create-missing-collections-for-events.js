6e3263a57e0ed9b7ef135b3aad1c7306c5195fb9
Nikita Melnikov <nikmel2803@gmail.com>
2020-07-03 02:59:58 +0300

    add docs to migration

--- migrations/20200702185200-create-missing-collections-for-events.js
+++ migrations/20200702185200-create-missing-collections-for-events.js
@@ -1,5 +1,10 @@
 /**
  * @file Migration for creating collections for storing events and setup indexes for them
+ * The problem:
+ *  not for all projects there are corresponding collections for storing events (events, repetitions, dailyEvents)
+ *
+ * The solution:
+ *  Create necessary collections and setup indexes for them
  */
 const dotenv = require('dotenv');
 const path = require('path');
@@ -10,44 +15,35 @@ const REPETITIONS_GROUP_HASH_INDEX_NAME = 'groupHash_hashed';
 const REPETITIONS_USER_ID_INDEX_NAME = 'userId';
 
 /**
- * Asynchronous forEach function
- *
- * @param array - array to iterate
- * @param callback - callback for processing array items
+ * Read env variables
  */
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
-
-/**
- * Returns true if collection exists in database
- *
- * @param db - database to check
- * @param collectionName - collection name to check
- */
-async function isCollectionExists(db, collectionName) {
-  return db.listCollections({ name: collectionName }).hasNext();
-}
-
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
 });
 
 module.exports = {
-  async up(db, client) {
+  async up(db) {
+    /**
+     * Init connection to accounts DB
+     */
     const connection = await mongodb.MongoClient.connect(process.env.MONGO_ACCOUNTS_DATABASE_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
     const accountsDB = connection.db();
+
+    /**
+     * Get all projects from accounts DB
+     */
     const projects = await accountsDB.collection('projects').find()
       .toArray();
 
     console.log(`${projects.length} projects to check`);
 
+    /**
+     * Iterate over projects to create necessary collections and indexes
+     */
     await asyncForEach(projects, async (project) => {
       console.log(`Check project with _id ${project._id}`);
 
@@ -92,3 +88,25 @@ module.exports = {
   down() {},
 
 };
+
+/**
+ * Asynchronous forEach function
+ *
+ * @param array - array to iterate
+ * @param callback - callback for processing array items
+ */
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+/**
+ * Returns true if collection exists in database
+ *
+ * @param db - database to check
+ * @param collectionName - collection name to check
+ */
+async function isCollectionExists(db, collectionName) {
+  return db.listCollections({ name: collectionName }).hasNext();
+}
