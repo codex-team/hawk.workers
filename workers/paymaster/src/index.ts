a8f72d612e6c33f05c978abc0a20f007e7f1d198
ilyamore88 <themorozik@gmail.com>
2020-10-20 19:56:52 +0300

    add env checker, update codex-accounting-sdk version

--- workers/paymaster/src/index.ts
+++ workers/paymaster/src/index.ts
@@ -72,13 +72,28 @@ export default class PaymasterWorker extends Worker {
       throw new Error('Please specify ACCOUNTING_API_ENDPOINT in .env file');
     }
 
-    this.accounting = new Accounting({
-      baseURL: process.env.ACCOUNTING_API_ENDPOINT,
-      tlsVerify: {
+    /**
+     * Initializing accounting SDK
+     */
+    let tlsVerify;
+
+    /**
+     * Checking env variables
+     * If at least one path is not transmitted, the variable tlsVerify is undefined
+     */
+    if (
+      ![process.env.TLS_CA_CERT, process.env.TLS_CERT, process.env.TLS_KEY].some(value => value === undefined || value.length === 0)
+    ) {
+      tlsVerify = {
         tlsCaCertPath: `${process.env.TLS_CA_CERT}`,
         tlsCertPath: `${process.env.TLS_CERT}`,
         tlsKeyPath: `${process.env.TLS_KEY}`,
-      },
+      };
+    }
+
+    this.accounting = new Accounting({
+      baseURL: process.env.ACCOUNTING_API_ENDPOINT,
+      tlsVerify,
     });
 
     await super.start();
