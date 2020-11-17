1d1f7f9c28b7403a6e15d08e7d834a6429d7b03c
Nikita Melnikov <nikmel2803@gmail.com>
2020-11-18 01:00:09 +0300

    base limiter implementation

---
+++ workers/limiter/src/index.ts
@@ -0,0 +1,135 @@
+import {DatabaseController} from '../../../lib/db/controller';
+import {Worker} from '../../../lib/worker';
+import * as pkg from '../package.json';
+import asyncForEach from '../../../lib/utils/asyncForEach';
+import {Collection, Db} from 'mongodb';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+import {PlanDBScheme, ProjectDBScheme, WorkspaceDBScheme} from 'hawk.types';
+
+type WorkspaceWithTariffPlan = WorkspaceDBScheme & {tariffPlan: PlanDBScheme}
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+/**
+ *
+ */
+export default class LimiterWorker extends Worker {
+  /**
+   * Worker type
+   */
+  public readonly type: string = pkg.workerType;
+
+  /**
+   * Database Controller for events database
+   */
+  private eventsDb: DatabaseController = new DatabaseController(process.env.MONGO_EVENTS_DATABASE_URI);
+
+  /**
+   * Database Controller for accounts database
+   */
+  private accountsDb: DatabaseController = new DatabaseController(process.env.MONGO_ACCOUNTS_DATABASE_URI);
+
+  /**
+   * Connection to events DB
+   */
+  private eventsDbConnection!: Db;
+
+  /**
+   * Collection with projects
+   */
+  private projectsCollection!: Collection<ProjectDBScheme>;
+
+  /**
+   * Collection with workspaces
+   */
+  private workspacesCollection!: Collection<WorkspaceDBScheme>;
+
+  /**
+   * Start consuming messages
+   */
+  public async start(): Promise<void> {
+    this.eventsDbConnection = await this.eventsDb.connect();
+    const accountDbConnection = await this.accountsDb.connect();
+
+    this.projectsCollection = accountDbConnection.collection<ProjectDBScheme>('projects');
+    this.workspacesCollection = accountDbConnection.collection<WorkspaceDBScheme>('workspaces');
+    await super.start();
+  }
+
+  /**
+   * Finish everything
+   */
+  public async finish(): Promise<void> {
+    await super.finish();
+    await this.eventsDb.close();
+    await this.accountsDb.close();
+  }
+
+  /**
+   * Task handling function
+   */
+  public async handle(): Promise<void> {
+    const [projects, workspaces] = await Promise.all([
+      this.projectsCollection.find({}).toArray(),
+      this.workspacesCollection.aggregate<WorkspaceWithTariffPlan>([
+        {
+          $lookup: {
+            from: 'plans',
+            localField: 'tariffPlanId',
+            foreignField: '_id',
+            as: 'tariffPlan',
+          },
+        }, {
+          $unwind: {
+            path: '$tariffPlan',
+          },
+        },
+      ]).toArray(),
+    ]);
+    const workspacesMap = workspaces.reduce((acc, workspace) => {
+      acc[workspace._id.toString()] = workspace;
+
+      return acc;
+    }, {} as Record<string, WorkspaceWithTariffPlan>);
+
+    console.log(workspacesMap);
+
+    const workspaceEventsCount: Record<string, number> = {};
+
+    await asyncForEach(projects, async (project) => {
+      this.logger.info('Processing project with id ' + project._id);
+      const workspace = workspacesMap[project.workspaceId.toString()];
+
+      if (!workspace.lastChargeDate) {
+        return;
+      }
+
+      const repetitionsCollection = this.eventsDbConnection.collection('repetitions:' + project._id);
+      const eventsCollection = this.eventsDbConnection.collection('events:' + project._id);
+
+      const query = {
+        'payload.timestamp': {
+          $gt: Math.floor(new Date(workspace.lastChargeDate).getTime() / 1000),
+        },
+      };
+
+      const [repetitionsCount, originalEventCount] = await Promise.all([repetitionsCollection.find(query).count(), eventsCollection.find(query).count()]);
+
+      workspaceEventsCount[project.workspaceId.toString()] = (workspaceEventsCount[project.workspaceId.toString()] || 0) + repetitionsCount + originalEventCount;
+    });
+
+    await asyncForEach(Object.entries(workspaceEventsCount), async ([workspaceId, eventsCount]) => {
+      const workspace = workspacesMap[workspaceId];
+
+      await this.workspacesCollection.updateOne(
+        { _id: workspace._id },
+        { $set: { billingPeriodEventsCount: eventsCount } }
+      );
+
+      if (workspace.tariffPlan.eventsLimit < eventsCount) {
+        // @todo наложить в редис
+      }
+    });
+  }
+}
---
+++ workers/limiter/tests/index.test.ts
@@ -0,0 +1,10 @@
+import LimiterWorker from "../src";
+
+describe('Limiter worker', () => {
+  const worker = new LimiterWorker();
+
+  test('Should save current events count for workspace from last charge date', () => {
+
+  });
+  test.skip('Should ban projects in workspace that exceed the limit of events on tariff plan');
+});
