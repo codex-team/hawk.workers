ca3e839100a06b4d309059b1cfd7c6fa9d291b63
Taly <vitalik7tv@yandex.ru>
2020-11-26 19:13:29 +0300

    remove comments

--- workers/javascript/src/index.ts
+++ workers/javascript/src/index.ts
@@ -118,18 +118,6 @@ export default class JavascriptEventWorker extends EventWorker {
       return this.cache.get(
         `consumeBacktraceFrame:${event.payload.release.toString()}:${Crypto.hash(frame)}:${index}`,
         () => {
-          // /**
-          //  * Frame data is missing
-          //  */
-          // if (!frame.file) {
-          //   return Object.assign({
-          //     line: 0,
-          //     column: 0,
-          //     file: '',
-          //     sourceCode: [],
-          //   }, frame) as BacktraceFrame;
-          // }
-
           return this.consumeBacktraceFrame(frame, releaseRecord)
             .catch((error) => {
               this.logger.error('Error while consuming ' + error.stack);
@@ -213,11 +201,6 @@ export default class JavascriptEventWorker extends EventWorker {
        * Get 5 lines above and 5 below
        */
       lines = this.readSourceLines(consumer, originalLocation);
-    // } else {
-    //   HawkCatcher.send(new Error('JS Worker (parsing maps): originalLocation.source is missing'), {
-    //     stackFrame,
-    //     originalLocation,
-    //   });
     }
 
     consumer.destroy();
