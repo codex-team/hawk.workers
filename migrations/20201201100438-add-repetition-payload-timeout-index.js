6374a0a430e2ca0144029bbb6f569eebead009b7
N0str <ntpcp@yandex.ru>
2020-12-01 13:07:03 +0300

    Add migration to the repetitions collection on payload.timeout

---
+++ migrations/20201201100438-add-repetition-payload-timeout-index.js
@@ -0,0 +1,77 @@
+/**
+ * @file This migration creates indexes for repetition collection on payload.timeout field
+ */
+
+/**
+ * Index name for payload.timeout field
+ */
+const payloadTimeoutIndexName = 'payloadTimeout';
+
+module.exports = {
+  async up(db) {
+    const collections = await db.listCollections({}, {
+      authorizedCollections: true,
+      nameOnly: true,
+    }).toArray();
+
+    const targetCollections = [];
+
+    collections.forEach((collection) => {
+      if (/repetitions/.test(collection.name)) {
+        targetCollections.push(collection.name);
+      }
+    });
+
+    console.log('Start adding indexes to payload.timeout');
+    console.log(`${targetCollections.length} collections will be updated.`);
+
+    let currentCollectionNumber = 1;
+
+    for (const collectionName of targetCollections) {
+      const hasIndexAlready = await db.collection(collectionName).indexExists(payloadTimeoutIndexName);
+
+      console.log(`${currentCollectionNumber} of ${targetCollections.length} in process.`);
+
+      if (!hasIndexAlready) {
+        await db.collection(collectionName).createIndex({
+          'payload.user.id': 1,
+        }, {
+          name: payloadTimeoutIndexName,
+          sparse: true,
+        });
+        console.log('Create index', collectionName);
+      } else {
+        console.log('Skip', collectionName);
+      }
+
+      currentCollectionNumber++;
+    }
+  },
+  async down(db) {
+    const collections = await db.listCollections({}, {
+      authorizedCollections: true,
+      nameOnly: true,
+    }).toArray();
+
+    const targetCollections = [];
+
+    collections.forEach((collection) => {
+      if (/repetitions/.test(collection.name)) {
+        targetCollections.push(collection.name);
+      }
+    });
+
+    console.log('Start dropping indexes to payload.timeout');
+    console.log(`${targetCollections.length} collections will be updated.`);
+
+    let currentCollectionNumber = 1;
+
+    for (const collectionName of targetCollections) {
+      console.log(`${currentCollectionNumber} of ${targetCollections.length} in process.`);
+
+      await db.collection(collectionName).dropIndex(payloadTimeoutIndexName);
+      console.log('Index dropped', collectionName);
+      currentCollectionNumber++;
+    }
+  },
+};
