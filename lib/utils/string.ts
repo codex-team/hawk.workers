deb2f14b118c1ccb50b089611b4e6fe58ca40b3f
Peter Savchenko <specc.dev@gmail.com>
2021-02-19 20:50:37 +0300

    fix(js): trim source code lines

---
+++ lib/utils/string.ts
@@ -0,0 +1,18 @@
+/**
+ * String helpers
+ */
+
+/**
+ * Trims source code line content to max length
+ * Adds «...» in case of trimming
+ *
+ * @param content - content to trim
+ * @param maxLen - trimming criteria
+ */
+export function rightTrim(content: string, maxLen = 140): string {
+  if (content.length <= maxLen) {
+    return content;
+  }
+
+  return content.substr(0, maxLen)  + '…';
+}
