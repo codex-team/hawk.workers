b845f6eaf36d5bfed92c4dcc4e95487e9ab742da
Taly <vitalik7tv@yandex.ru>
2021-03-31 17:58:57 +0300

    do not touch nodejs worker

--- workers/nodejs/src/index.ts
+++ workers/nodejs/src/index.ts
@@ -1,4 +1,6 @@
 import { EventWorker } from '../../../lib/event-worker';
+import * as WorkerNames from '../../../lib/workerNames';
+import { GroupWorkerTask } from '../../grouper/types/group-worker-task';
 import * as pkg from '../package.json';
 import { NodeJSEventWorkerTask } from '../types/nodejs-event-worker-task';
 
@@ -17,6 +19,23 @@ export default class NodeJSEventWorker extends EventWorker {
    * @param event - event to handle
    */
   public async handle(event: NodeJSEventWorkerTask): Promise<void> {
-    return super.handle(event);
+    this.validate(event);
+
+    await this.addTask(WorkerNames.GROUPER, {
+      projectId: event.projectId,
+      catcherType: this.type,
+      event: event.payload,
+    } as GroupWorkerTask);
+  }
+
+  /**
+   * Validate passed event data
+   *
+   * @param {NodeJSEventWorkerTask} event - event to be validated
+   */
+  private validate(event: NodeJSEventWorkerTask): void {
+    if (!event.projectId || !event.payload) {
+      throw new Error('Bad data was given');
+    }
   }
 }
