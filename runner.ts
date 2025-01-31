52bb4f6cd4e7cdbcf1c03a6d26b2a0b847411f01
Peter <specc.dev@gmail.com>
2025-01-31 20:11:53 +0300

    chore(logs): add logs and try catch blocks (#347)
    * Update redisHelper.ts
    
    * Update runner.ts
    
    * try-catch and logs to runner and redisHelper
    
    * Update runner.ts

--- runner.ts
+++ runner.ts
@@ -166,7 +166,13 @@ class WorkerRunner {
    */
   private constructWorkers(workerConstructors: WorkerConstructor[]): void {
     return workerConstructors.forEach((WorkerClass) => {
-      this.workers.push(new WorkerClass());
+      try {
+        const worker = new WorkerClass();
+
+        this.workers.push(worker);
+      } catch (error) {
+        console.error('Error constructing worker', error);
+      }
     });
   }
 
@@ -296,5 +302,9 @@ class WorkerRunner {
   }
 }
 
-// eslint-disable-next-line no-new
-new WorkerRunner();
+try {
+  // eslint-disable-next-line no-new
+  new WorkerRunner();
+} catch (error) {
+  console.error('Error running worker runner', error);
+}
