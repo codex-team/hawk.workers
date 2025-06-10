86e05bf2a604fd843cc11e61b13c5c1e53860cc4
e11sy <130844513+e11sy@users.noreply.github.com>
2025-06-10 15:26:20 +0300

    fix(release): delete inside of the Promise.all

--- workers/release/src/index.ts
+++ workers/release/src/index.ts
@@ -179,16 +179,22 @@ export default class ReleaseWorker extends Worker {
             const fileInfo = await this.saveFile(map);
 
             /**
-             * Remove 'content' and save id of saved file instead
+             * Save id of saved file instead
              */
             map._id = fileInfo._id;
-            delete map.content;
 
             return map;
           } catch (error) {
             this.logger.error(`Map ${map.mapFileName} was not saved: ${error}`);
           }
         }));
+
+        /**
+         * Delete file content after it is saved to the GridFS
+         */
+        savedFiles.forEach(file => {
+          delete file.content;
+        })
 
         /**
          * Filter unsaved maps
