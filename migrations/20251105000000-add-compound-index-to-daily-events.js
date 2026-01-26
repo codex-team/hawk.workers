f5f3aae0e9ac7bf3e07eb2d3b00f710d81bad04b
Peter <specc.dev@gmail.com>
2026-01-26 21:31:07 +0300

    chore(setup): migrate from node 16 to 24 (#512)
    * Upgrade to Node.js 24 and update TypeScript config
    
    Update all workflows, Dockerfiles, and .nvmrc to use Node.js 24 for consistency and latest features. Enable 'moduleResolution: node' and 'skipLibCheck' in tsconfig.json to improve TypeScript compatibility and speed up builds.
    
    * upd packages
    
    * Update jest.setup.redis-mock.js
    
    * Update jest.global-teardown.js
    
    * fix tests
    
    * Update jest.setup.redis-mock.js
    
    * disable redis tests
    
    * rm skip
    
    * Update jest.config.js
    
    * rm skips
    
    * lint code

--- migrations/20251105000000-add-compound-index-to-daily-events.js
+++ migrations/20251105000000-add-compound-index-to-daily-events.js
@@ -5,90 +5,88 @@
 const indexName = 'groupingTimestampAndLastRepetitionTimeAndId';
 
 module.exports = {
-    async up(db) {
-        const collections = await db.listCollections({}, {
-            authorizedCollections: true,
-            nameOnly: true,
-        }).toArray();
+  async up(db) {
+    const collections = await db.listCollections({}, {
+      authorizedCollections: true,
+      nameOnly: true,
+    }).toArray();
 
-        const targetCollections = [];
+    const targetCollections = [];
 
-        collections.forEach((collection) => {
-            if (/dailyEvents:/.test(collection.name)) {
-                targetCollections.push(collection.name);
-            }
-        });
+    collections.forEach((collection) => {
+      if (/dailyEvents:/.test(collection.name)) {
+        targetCollections.push(collection.name);
+      }
+    });
 
-        console.log(`${targetCollections.length} dailyEvents collections will be updated.`);
+    console.log(`${targetCollections.length} dailyEvents collections will be updated.`);
 
-        let currentCollectionNumber = 1;
+    let currentCollectionNumber = 1;
 
-        for (const collectionName of targetCollections) {
-            console.log(`${collectionName} in process.`);
-            console.log(`${currentCollectionNumber} of ${targetCollections.length} in process.`);
+    for (const collectionName of targetCollections) {
+      console.log(`${collectionName} in process.`);
+      console.log(`${currentCollectionNumber} of ${targetCollections.length} in process.`);
 
-            try {
-                const hasIndexAlready = await db.collection(collectionName).indexExists(indexName);
+      try {
+        const hasIndexAlready = await db.collection(collectionName).indexExists(indexName);
 
-                if (!hasIndexAlready) {
-                    await db.collection(collectionName).createIndex({
-                        groupingTimestamp: -1,
-                        lastRepetitionTime: -1,
-                        _id: -1,
-                    }, {
-                        name: indexName,
-                        background: true,
-                    });
-                    console.log(`Index ${indexName} created for ${collectionName}`);
-                } else {
-                    console.log(`Index ${indexName} already exists for ${collectionName}`);
-                }
-            } catch (error) {
-                console.error(`Error adding index to ${collectionName}:`, error);
-            }
-
-            currentCollectionNumber++;
+        if (!hasIndexAlready) {
+          await db.collection(collectionName).createIndex({
+            groupingTimestamp: -1,
+            lastRepetitionTime: -1,
+            _id: -1,
+          }, {
+            name: indexName,
+            background: true,
+          });
+          console.log(`Index ${indexName} created for ${collectionName}`);
+        } else {
+          console.log(`Index ${indexName} already exists for ${collectionName}`);
         }
-    },
+      } catch (error) {
+        console.error(`Error adding index to ${collectionName}:`, error);
+      }
 
-    async down(db) {
-        const collections = await db.listCollections({}, {
-            authorizedCollections: true,
-            nameOnly: true,
-        }).toArray();
-
-        const targetCollections = [];
-
-        collections.forEach((collection) => {
-            if (/dailyEvents:/.test(collection.name)) {
-                targetCollections.push(collection.name);
-            }
-        });
-
-        console.log(`${targetCollections.length} dailyEvents collections will be updated.`);
-
-        let currentCollectionNumber = 1;
-
-        for (const collectionName of targetCollections) {
-            console.log(`${collectionName} in process.`);
-            console.log(`${currentCollectionNumber} of ${targetCollections.length} in process.`);
-
-            try {
-                const hasIndexAlready = await db.collection(collectionName).indexExists(indexName);
-
-                if (hasIndexAlready) {
-                    await db.collection(collectionName).dropIndex(indexName);
-                    console.log(`Index ${indexName} dropped for ${collectionName}`);
-                } else {
-                    console.log(`Index ${indexName} does not exist for ${collectionName}, skipping drop.`);
-                }
-            } catch (error) {
-                console.error(`Error dropping index from ${collectionName}:`, error);
-            }
-
-            currentCollectionNumber++;
-        }
+      currentCollectionNumber++;
     }
+  },
+
+  async down(db) {
+    const collections = await db.listCollections({}, {
+      authorizedCollections: true,
+      nameOnly: true,
+    }).toArray();
+
+    const targetCollections = [];
+
+    collections.forEach((collection) => {
+      if (/dailyEvents:/.test(collection.name)) {
+        targetCollections.push(collection.name);
+      }
+    });
+
+    console.log(`${targetCollections.length} dailyEvents collections will be updated.`);
+
+    let currentCollectionNumber = 1;
+
+    for (const collectionName of targetCollections) {
+      console.log(`${collectionName} in process.`);
+      console.log(`${currentCollectionNumber} of ${targetCollections.length} in process.`);
+
+      try {
+        const hasIndexAlready = await db.collection(collectionName).indexExists(indexName);
+
+        if (hasIndexAlready) {
+          await db.collection(collectionName).dropIndex(indexName);
+          console.log(`Index ${indexName} dropped for ${collectionName}`);
+        } else {
+          console.log(`Index ${indexName} does not exist for ${collectionName}, skipping drop.`);
+        }
+      } catch (error) {
+        console.error(`Error dropping index from ${collectionName}:`, error);
+      }
+
+      currentCollectionNumber++;
+    }
+  },
 };
-
-
